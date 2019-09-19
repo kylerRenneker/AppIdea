f5906c370058752dbdecca3ea645e521864ad136
Kyler <Krenneker16@gmail.com>
2019-09-19 08:21:24 -0400

    changes to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,53 @@
-require('dotenv').config()
-const { DB_URL } = require('./src/config')
-
-const pg = require('pg')
-//pg.defaults.ssl = true
+// Update with your config settings.
 
 module.exports = {
-  client: 'pg',
-  connection: 'postgresql://postgres@localhost:5432/AppIdea'
-}
+
+  development: {
+    client: 'pg',
+    connection: 'postgresql://postgres@localhost:5432/AppIdea'
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
+
+// require('dotenv').config()
+// const { DB_URL } = require('./src/config')
+
+// const pg = require('pg')
+// //pg.defaults.ssl = true
+
+// module.exports = {
+//   client: 'pg',
+//   connection: 'postgresql://postgres@localhost:5432/AppIdea'
+// }
