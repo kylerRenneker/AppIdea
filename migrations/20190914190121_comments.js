0fe522717b2479caa87f770b12fb126a882868f4
Kyler Renneker <Krenneker16@gmail.com>
2019-09-21 11:21:23 -0400

    added migration and base seed file for comments

--- migrations/20190914190121_comments.js
+++ migrations/20190914190121_comments.js
@@ -3,10 +3,13 @@ exports.up = function (knex) {
     return knex.schema
         .createTable('comments', function (table) {
             table.increments('id').primary()
-            table.string('', 255).notNullable
+            table.string('text', 2055).notNullable
+            table.integer('user_id').references('users.id')
+            table.integer('idea_id').references('ideas.id')
         })
 };
 
 exports.down = function (knex) {
-
+    return knex.schema
+        .dropTable('comments')
 };
